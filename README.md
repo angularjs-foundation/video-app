79d9297401346d2b90039684b2ef2fb5bbd18b3a
simpulton <simpul@gmail.com>
2014-04-15 10:31:14 -0700

    Updating exercises

--- README.md
+++ README.md
@@ -5,7 +5,24 @@
 2. Run `grunt` to start the application.
 
 ## Exercises
-1. Run the tests using `grunt autotest` and complete the JavaScript code to make the test work inside of `scripts/ntApp.js`.
-  - Implement a scrolling directive that contains a scroll event that is fired upon `$routeChangeStart`.
-  - Implement the ntProgress directive functionality that is performed within `ntLoadingIndicator`.
-  - Implement the directive definition properties that `ntFakeVideo` expects to work.
+Run the tests using `grunt test` and complete the exercises until all tests are passing.
+
+### M6.1 Your First Directive
+Create a listener for the `$routeChangeStart` event in `ntScrollToTop`.
+
+Add the code to scroll to the top of the page when `$routeChangeStart` is fired.
+
+### M6.2 Directive Definition Object
+Create a directive definition object (DDO) on `ntFakeVideo`.
+
+Add a `template` property to the DDO to define the markup for `ntFakeVideo`.
+// Elaborate. What is the markup supposed to look like?
+
+Add a `scope` property to the DDO to reads the values off of the HTML attributes.
+// Elaborate. What are we listening for? Wouldn't this be a better example for the use of attrs in a link function?
+
+### M6.3 Link Function
+
+### M6.4 Controller Function
+
+
--- app/scripts/ntApp.js
+++ app/scripts/ntApp.js
@@ -1,205 +1,210 @@
 angular.module('ntApp', ['ytCore', 'ngRoute', 'ntAnimations'])
 
-  .constant('TPL_PATH', './templates')
+    .constant('TPL_PATH', './templates')
 
-  .run(['$rootScope', 'TPL_PATH', function($rootScope,   TPL_PATH) {
-    $rootScope.tpl = function(file) {
-      return TPL_PATH + '/' + file + '.html';
-    };
+    .run(['$rootScope', 'TPL_PATH', function ($rootScope, TPL_PATH) {
+        $rootScope.tpl = function (file) {
+            return TPL_PATH + '/' + file + '.html';
+        };
 
-    $rootScope.$on('$routeChangeStart', function() {
-      $rootScope.$broadcast('ntLoadingStart');
-    });
-  }])
+        $rootScope.$on('$routeChangeStart', function () {
+            $rootScope.$broadcast('ntLoadingStart');
+        });
+    }])
 
 
-  .factory('getSet', function() {
-    return function() {
-      var val;
-      return function(data) {
-        return arguments.length ? (val = data) : val;
-      };
-    };
-  })
+    .factory('getSet', function () {
+        return function () {
+            var val;
+            return function (data) {
+                return arguments.length ? (val = data) : val;
+            };
+        };
+    })
 
-  .value('appCategories', [
-    'funny','programming', 'web development',
-    'music', 'video games'
-  ])
+    .value('appCategories', [
+        'funny', 'programming', 'web development',
+        'music', 'video games'
+    ])
 
-  .config(function($routeProvider, TPL_PATH) {
-    $routeProvider.when('/',{
-      controller : 'HomeCtrl',
-      templateUrl : TPL_PATH + '/home.html',
-      reloadOnSearch : false
-    }).when('/watch/:id',{
-      controller : 'WatchCtrl',
-      templateUrl : TPL_PATH + '/watch.html',
-      resolve: {
-        videoInstance: ['ytVideo', '$location', function(ytVideo, $location) {
-          //match the ID with a regex instead of using route params
-          //since the route has not fully changed yet
-          var id = $location.path().match(/watch\/([^ \/]+)(\/|$)/)[1];
-          return ytVideo(id);
-        }]
-      }
-    });
-  })
+    .config(function ($routeProvider, TPL_PATH) {
+        $routeProvider.when('/', {
+            controller: 'HomeCtrl',
+            templateUrl: TPL_PATH + '/home.html',
+            reloadOnSearch: false
+        }).when('/watch/:id', {
+            controller: 'WatchCtrl',
+            templateUrl: TPL_PATH + '/watch.html',
+            resolve: {
+                videoInstance: ['ytVideo', '$location', function (ytVideo, $location) {
+                    //match the ID with a regex instead of using route params
+                    //since the route has not fully changed yet
+                    var id = $location.path().match(/watch\/([^ \/]+)(\/|$)/)[1];
+                    return ytVideo(id);
+                }]
+            }
+        });
+    })
 
-  .directive('ntScrollToTop', ['$window', '$rootScope', function($window, $rootScope) {
-    return function() {
-      //implement here with $window.scrollTo(0, 0);
-    };
-  }])
+    .directive('ntScrollToTop', ['$window', '$rootScope', function ($window, $rootScope) {
+        return function () {
+            // [M6.1] Create a listener for the `$routeChangeStart` event in `ntScrollToTop`.
 
-  .service('ntProgress', function() {
-    return NProgress;
-  })
+            // [M6.1] Add the code to scroll to the top of the page when `$routeChangeStart` is fired.
+            // HINT $window.scrollTo(0, 0);
+        };
+    }])
 
-  .directive('ntLoadingIndicator', ['ntProgress', function(ntProgress) {
-    return {
-      //implement on within a controller
-      //ntProgress.configure({ ease: 'ease', speed: 500 });
-      //on scope.start() and listen on scope.$on('ntLoadingStart');
-      //ntProgress.start();
-      //on scope.start() and listen on scope.$on('ntLoadingEnd');
-      //ntProgress.done();
-    };
-  }])
+    .service('ntProgress', function () {
+        return NProgress;
+    })
 
-  .directive('ntFakeVideo', [function() {
-    return {
-      //implement on using scope : {} and template : '...'
-      //use =var and/or @var to read values off of the attributes
-    };
-  }])
+    .directive('ntLoadingIndicator', ['ntProgress', function (ntProgress) {
+        return {
+            //implement on within a controller
+            //ntProgress.configure({ ease: 'ease', speed: 500 });
+            //on scope.start() and listen on scope.$on('ntLoadingStart');
+            //ntProgress.start();
+            //on scope.start() and listen on scope.$on('ntLoadingEnd');
+            //ntProgress.done();
+        };
+    }])
 
-  .factory('currentVideo', ['getSet', function(getSet) {
-    return getSet();
-  }])
+    .directive('ntFakeVideo', [function () {
+        // [M6.2] Create a directive definition object (DDO) on `ntFakeVideo`.
 
-  .controller('HomeCtrl', ['$scope', '$rootScope', '$location', 'ytSearch', 'ytFeed', 'TPL_PATH',
-                   function($scope,   $rootScope,   $location,   ytSearch,   ytFeed,   TPL_PATH) {
+        // [M6.2] Add a `template` property to the DDO to define the markup for `ntFakeVideo`.
 
-    var layout;
-    $scope.setLayout = function(l) {
-      layout = l;
-    };
+        // [M6.2] Create a directive definition object (DDO) on `ntFakeVideo`.
+        // HINT Use '=var and/or @var to read values off of the attributes
+    }])
 
-    $scope.isLayout = function(l) {
-      return layout == l;
-    };
+    .factory('currentVideo', ['getSet', function (getSet) {
+        return getSet();
+    }])
 
-    function hasLocationChanged() {
-      return $location.path().indexOf('watch') >= 0;
-    };
+    .controller('HomeCtrl', ['$scope', '$rootScope', '$location', 'ytSearch', 'ytFeed', 'TPL_PATH',
+        function ($scope, $rootScope, $location, ytSearch, ytFeed, TPL_PATH) {
 
-    $scope.$watchCollection(function() {
-      return $location.search();
-    }, function(data) {
+            var layout;
+            $scope.setLayout = function (l) {
+                layout = l;
+            };
 
-      //do not reload the results if the location changed to
-      //the watch page
-      if(hasLocationChanged()) return;
+            $scope.isLayout = function (l) {
+                return layout == l;
+            };
 
-      $scope.setLayout('pictures');
+            function hasLocationChanged() {
+                return $location.path().indexOf('watch') >= 0;
+            };
 
-      var c = data.c;
-      if(c && c.length > 0) {
-        $scope.searchTerm = c;
-        $scope.searchMethod = 'category';
-      } else {
-        data.q = data.q || 'AngularJS';
-        $scope.searchMethod = 'query';
-        $scope.searchTerm = data.q;
-      }
+            $scope.$watchCollection(function () {
+                return $location.search();
+            }, function (data) {
 
-      $rootScope.$broadcast('ntLoadingStart');
+                //do not reload the results if the location changed to
+                //the watch page
+                if (hasLocationChanged()) return;
 
-      ytSearch(data).then(function(videos) {
-        $scope.videos = videos;
-        $rootScope.$broadcast('ntLoadingEnd');
-      });
+                $scope.setLayout('pictures');
 
-      ytFeed('most_popular').then(function(videos) {
-        $scope.popularVideos = videos;
-      });
-    });
-  }])
+                var c = data.c;
+                if (c && c.length > 0) {
+                    $scope.searchTerm = c;
+                    $scope.searchMethod = 'category';
+                } else {
+                    data.q = data.q || 'AngularJS';
+                    $scope.searchMethod = 'query';
+                    $scope.searchTerm = data.q;
+                }
 
-  .filter('limit', function() {
-    return function(results, limit) {
-      return results && results.slice(0, limit);
-    };
-  })
+                $rootScope.$broadcast('ntLoadingStart');
 
-  .controller('CategoryListCtrl', ['$scope', 'appCategories',
-                           function($scope,   appCategories) {
-    $scope.categories = appCategories;
-  }])
+                ytSearch(data).then(function (videos) {
+                    $scope.videos = videos;
+                    $rootScope.$broadcast('ntLoadingEnd');
+                });
 
-  .controller('SearchFormCtrl', ['$scope', '$location',
-                         function($scope,   $location) {
+                ytFeed('most_popular').then(function (videos) {
+                    $scope.popularVideos = videos;
+                });
+            });
+        }])
 
-    $scope.search = function() {
-      var order, category, q = $scope.q;
-      if($scope.advanced) {
-        order = $scope.advanced.orderby;
-        category = $scope.advanced.category;
-      }
+    .filter('limit', function () {
+        return function (results, limit) {
+            return results && results.slice(0, limit);
+        };
+    })
 
-      $scope.advanced = false;
+    .controller('CategoryListCtrl', ['$scope', 'appCategories',
+        function ($scope, appCategories) {
+            $scope.categories = appCategories;
+        }])
 
-      $location.search({
-        q : q || '',
-        c : category || '',
-        o : order || ''
-      }).path('/');
-    };
+    .controller('SearchFormCtrl', ['$scope', '$location',
+        function ($scope, $location) {
 
-    $scope.$on('$routeChangeStart', function() {
-      $scope.advanced = false;
-    });
+            $scope.search = function () {
+                var order, category, q = $scope.q;
+                if ($scope.advanced) {
+                    order = $scope.advanced.orderby;
+                    category = $scope.advanced.category;
+                }
 
-    $scope.orderingOptions = [
-      'relevance',
-      'published',
-      'viewCount',
-      'rating',
-      'position',
-      'commentCount',
-      'published',
-      'reversedPosition',
-      'title',
-      'viewCount'
-    ];
-  }])
+                $scope.advanced = false;
 
-  .controller('WatchCtrl', ['$scope', '$rootScope', '$location',  'videoInstance', 'ytVideoComments', 'TPL_PATH', 'currentVideo', 'ytSearch', 'ytRelatedVideos',
-                    function($scope,   $rootScope,   $location,    videoInstance,   ytVideoComments,   TPL_PATH,   currentVideo,   ytSearch,   ytRelatedVideos) {
+                $location.search({
+                    q: q || '',
+                    c: category || '',
+                    o: order || ''
+                }).path('/');
+            };
 
-    var videoID = videoInstance.id;
-    $scope.video_id = videoID;
-    $scope.video = videoInstance;
+            $scope.$on('$routeChangeStart', function () {
+                $scope.advanced = false;
+            });
 
-    ytVideoComments(videoInstance.id).then(function(comments) {
-      $scope.video_comments = comments;
-    });
+            $scope.orderingOptions = [
+                'relevance',
+                'published',
+                'viewCount',
+                'rating',
+                'position',
+                'commentCount',
+                'published',
+                'reversedPosition',
+                'title',
+                'viewCount'
+            ];
+        }])
 
-    currentVideo(videoInstance);
+    .controller('WatchCtrl', ['$scope', '$rootScope', '$location', 'videoInstance', 'ytVideoComments', 'TPL_PATH', 'currentVideo', 'ytSearch', 'ytRelatedVideos',
+        function ($scope, $rootScope, $location, videoInstance, ytVideoComments, TPL_PATH, currentVideo, ytSearch, ytRelatedVideos) {
 
-    ytRelatedVideos(videoID).then(function(videos) {
-      $scope.relatedVideos = videos;
-      $rootScope.$broadcast('ntLoadingEnd');
-    });
+            var videoID = videoInstance.id;
+            $scope.video_id = videoID;
+            $scope.video = videoInstance;
 
-    $scope.$on('$destroy', function() {
-      currentVideo(null);
-    });
-  }])
+            ytVideoComments(videoInstance.id).then(function (comments) {
+                $scope.video_comments = comments;
+            });
 
-  .controller('VideoPanelCtrl', ['$scope', 'currentVideo',
-                         function($scope,   currentVideo) {
+            currentVideo(videoInstance);
 
-    $scope.video = currentVideo();
-  }]);
+            ytRelatedVideos(videoID).then(function (videos) {
+                $scope.relatedVideos = videos;
+                $rootScope.$broadcast('ntLoadingEnd');
+            });
+
+            $scope.$on('$destroy', function () {
+                currentVideo(null);
+            });
+        }])
+
+    .controller('VideoPanelCtrl', ['$scope', 'currentVideo',
+        function ($scope, currentVideo) {
+
+            $scope.video = currentVideo();
+        }]);

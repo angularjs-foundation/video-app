14464b0b3ebdcf92fcc0a7f193500e9ebf9de44a
Matias Niemelä <matias@yearofmoo.com>
2014-05-08 17:49:53 -0400

    Simplified test files

--- test/karma-unit.conf.js
+++ test/karma-unit.conf.js
@@ -1,11 +1,11 @@
 module.exports = function(config) {
   config.set({
     files : [
-      'app/lib/jquery/dist/jquery.js',
       'app/lib/angular/angular.js',
       'app/lib/angular-route/angular-route.js',
       'app/lib/angular-animate/angular-animate.js',
       'app/lib/angular-mocks/angular-mocks.js',
+      'app/scripts/ntAppAnimations.js',
       'app/scripts/ntApp.js',
       'app/scripts/ytCore.js',
       'app/scripts/app.js',
